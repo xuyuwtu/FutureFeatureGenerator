f42060d7560d1a4d5679e83c4a79c4784ccee99e
xuyuwtu <3082068984@qq.com>
2025-11-01 15:00:59 +0800

    attempt to fix occasional ArgumentException occurrences 2

--- FutureFeatureGenerator/StringCatch.cs
+++ FutureFeatureGenerator/StringCatch.cs
@@ -1,32 +1,19 @@
-﻿using System.Text;
+﻿using System.Collections.Concurrent;
 
 namespace FutureFeatureGenerator;
 
 internal class StringCache
 {
-    private Dictionary<string, string> cache = new(StringComparer.Ordinal);
-    private ReferenceList<string> references = new();
-    private Encoding _encoding;
-    public Encoding Encoding => _encoding;
-    public StringCache(Encoding? encoding = null)
+    private ConcurrentDictionary<string, string> cache = new(StringComparer.Ordinal);
+    public StringCache()
     {
-        _encoding = encoding ?? Encoding.UTF8;
     }
     public string GetOrAdd(string s)
     {
-        var index = references.IndexOf(s);
-        if (index != -1)
-        {
-            return references[index];
-        }
-        if (cache.TryGetValue(s, out var result))
-        {
-            return result;
-        }
-        cache.Add(s, s);
-        references.Add(s);
-        return s;
+        return cache.GetOrAdd(s, s);
     }
-    public int IndexOf(string s) => references.IndexOf(s);
-    public bool Exists(string s) => IndexOf(s) != -1;
+    public bool Exists(string s)
+    {
+        return cache.ContainsKey(s);
+    }
 }
